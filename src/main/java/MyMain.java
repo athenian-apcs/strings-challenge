da4da194990a93c381a61c40df0c53dd7c23e4b3
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-10-01 11:01:26 -0700

    Added a challenge problem.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class MyMain {
 
     /**
-     * Checks whether a given string exists as a complete word in a given string.
-     * @param word
-     * @param str
-     * @return
+     * Checks whether a word exists as a complete word in a given string (i.e. it isn't part of a larger word).
+     * <p>
+     * For example, <code>existsAsWord("hello", "hello world")</code> and <code>existsAsWord("cats", "I like cats.")
+     * </code> should return true but <code>existsAsWord("taco", "tacocat")</code> and
+     * <code>existsAsWord("cat", "tacocat")</code> should return false.
+     * @param word The word to check for in the string. Case-sensitive. Guaranteed not to be empty or null, and to
+     *             contain only letters.
+     * @param str The string in which to check for the word. Case-sensitive. Guaranteed not to be null.
+     * @return Whether the word exists as a complete word in the string.
      */
     public static boolean existsAsWord (String word, String str) {
         return false;
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -4,7 +4,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
+
+    @Test
+    public void testExistsAsWord() {
+        assertTrue(MyMain.existsAsWord("taco", "I would love to eat a taco right now."));
+        assertTrue(MyMain.existsAsWord("Boxes", "Boxes are often used to contain packages."));
+        assertTrue(MyMain.existsAsWord("cat", "But is a tacocat really a cat?"));
+        assertTrue(MyMain.existsAsWord("life", "life"));
+
+        assertFalse(MyMain.existsAsWord("taco", "tacocat"));
+        assertFalse(MyMain.existsAsWord("word", ""));
+        assertFalse(MyMain.existsAsWord("bit", "Frodo is a hobbit from the shire."));
+        assertFalse(MyMain.existsAsWord("Jumps", "Jesper jumps constantly."));
+    }
 }
